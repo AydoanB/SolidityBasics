c43c3209f12db85cfdbf720411ff49fa447e8616
AydoanB <balakchiev02@gmail.com>
2023-03-25 12:22:10 +0200

    Add custom IERC20 token implementation

---
+++ contracts/ERC20.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns(uint);
+    function balanceOf(address account) external view returns(uint);
+    function transfer(address recipient, uint amount) external returns(bool);
+    function allowance(address owner, address spender) external view returns(uint);
+    function approve(address spender, uint amount) external returns(bool);
+    function transferFrom(address from, address to, uint amount) external returns(bool);
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+    event Approve(address indexed owner, address indexed spender, uint amount);
+}
+
+contract ERC20 is IERC20{
+    string constant ERROR_MESSAGE = "not enough tokens";
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+    mapping (address => mapping(address => uint)) public allowance;
+
+    string public name = "Test";
+    string public symbol = "TEST";
+    uint8 public decimals = 18;
+
+    function transfer(address recipient, uint amount) external returns(bool){
+        require(recipient != address(0), "zero address");
+        
+        require(balanceOf[msg.sender] >= amount, ERROR_MESSAGE);
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns(bool){
+        require(balanceOf[msg.sender] >= amount, ERROR_MESSAGE);
+        allowance[msg.sender][spender] += amount;
+
+        emit Approve(msg.sender, spender, amount);
+        
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint amount) external returns(bool){
+        require(balanceOf[from] >= amount, ERROR_MESSAGE);
+        require(to != address(0), "zero address");
+
+        allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+
+       return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external{
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
